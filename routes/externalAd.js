e7da7e693003728d73453e07d75fd587649d45ef
MohammadSalman1998 <mohammad.salman.m1998@gmail.com>
2025-03-15 00:51:02 +0300

    edit get ex_ADs

--- routes/externalAd.js
+++ routes/externalAd.js
@@ -4,7 +4,7 @@ const { upload,createExternalAd, getAllExternalAds, getExternalAdById, updateExt
 
 const router = express.Router();
 router.post("/", auth(), upload, createExternalAd);
-router.get("/", auth(), getAllExternalAds);
+router.get("/", getAllExternalAds);
 router.get("/:id", auth(), getExternalAdById);
 router.put("/:id", auth(), upload, updateExternalAd);
 router.delete("/:id", auth(), deleteExternalAd);
